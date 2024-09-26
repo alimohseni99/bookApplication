cb56cb7934564859dafff27b8a5f56a808808f0f
Ali Mohseni <ali.mohseni05@yahoo.se>
2024-09-26 20:56:21 +0200

    frontend done

---
+++ db.js
@@ -0,0 +1,64 @@
+const books = [
+  {
+    title: 'The Great Gatsby',
+    author: 'F. Scott Fitzgerald',
+    year: 1925,
+    genre: 'Fiction',
+  },
+  {
+    title: 'To Kill a Mockingbird',
+    author: 'Harper Lee',
+    year: 1960,
+    genre: 'Fiction',
+  },
+  {
+    title: '1984',
+    author: 'George Orwell',
+    year: 1949,
+    genre: 'Dystopian',
+  },
+  {
+    title: 'Pride and Prejudice',
+    author: 'Jane Austen',
+    year: 1813,
+    genre: 'Romance',
+  },
+  {
+    title: 'The Catcher in the Rye',
+    author: 'J.D. Salinger',
+    year: 1951,
+    genre: 'Fiction',
+  },
+  {
+    title: 'The Hobbit',
+    author: 'J.R.R. Tolkien',
+    year: 1937,
+    genre: 'Fantasy',
+  },
+  {
+    title: 'Moby Dick',
+    author: 'Herman Melville',
+    year: 1851,
+    genre: 'Adventure',
+  },
+  {
+    title: 'War and Peace',
+    author: 'Leo Tolstoy',
+    year: 1869,
+    genre: 'Historical',
+  },
+  {
+    title: 'The Odyssey',
+    author: 'Homer',
+    year: '8th century BC',
+    genre: 'Epic',
+  },
+  {
+    title: 'Crime and Punishment',
+    author: 'Fyodor Dostoevsky',
+    year: 1866,
+    genre: 'Philosophical',
+  },
+];
+
+module.exports = { books };
